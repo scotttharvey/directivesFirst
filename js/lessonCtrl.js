99b96cbfcaf0942f394c61a63cfbcd81f2411b76
Scott Harvey <scottyharvey33@gmil.com>
2016-05-31 17:30:54 -0600

    dolled up

--- js/lessonCtrl.js
+++ js/lessonCtrl.js
@@ -1,7 +1,11 @@
 angular.module('directivePractice').controller("lessonCtrl", function($scope){
   $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
   $scope.announceDay = function(lesson, day){
+    if(day === undefined){
+      alert("Wait and find out, silly  :)")
+  } else{
     alert(lesson + 'is active on ' + day + '.');
+  }
   }
 
 })
